fe8ed20b95874e39eb0224f749dbaddcb7830af5
Aniket Kumar <aniket787068@gmail.com>
2025-07-22 15:56:47 +0530

    change port

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import { connectDB } from './config/db.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // Check in deployment
 app.get('/ping', (req, res) => {
